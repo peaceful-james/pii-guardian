774bc5f1237a4ad6b198315a3bca2f60b8d5b796
Peaceful James <66864163+peaceful-james@users.noreply.github.com>
2025-04-04 18:56:32 +0100

    Receive slack events (#1)
    * install slack_elixir
    
    * skeleton slack_elixir code
    
    * make the slack events websocket work

---
+++ pii_guardian/lib/pii_guardian/slackbot.ex
@@ -0,0 +1,19 @@
+defmodule PiiGuardian.Slackbot do
+  @moduledoc false
+  use Slack.Bot
+
+  require Logger
+
+  @impl true
+  def handle_event("message", %{"channel" => channel, "text" => text, "user" => user}, _bot) do
+    Logger.info("Received message from user: #{user}")
+    if String.match?(text, ~r/hello/i) do
+      send_message(channel, "Hello! <@#{user}>")
+    end
+  end
+
+  def handle_event(type, payload, _bot) do
+    Logger.warning("Unhandled #{type} event: #{inspect(payload)}")
+    :ok
+  end
+end
