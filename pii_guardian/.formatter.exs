36f0b09d742237579030e315c3c63ca0abee779c
James Gough <james@peacefulprogramming.dev>
2025-04-04 18:07:59 +0100

    add formatting libs

--- pii_guardian/.formatter.exs
+++ pii_guardian/.formatter.exs
@@ -1,5 +1,10 @@
 [
   import_deps: [:ecto, :ecto_sql, :phoenix],
   subdirectories: ["priv/*/migrations"],
+  plugins: [
+    TailwindFormatter,
+    Styler,
+    Green.Lexmag.ElixirStyleGuideFormatter
+  ],
   inputs: ["*.{ex,exs}", "{config,lib,test}/**/*.{ex,exs}", "priv/*/seeds.exs"]
 ]
