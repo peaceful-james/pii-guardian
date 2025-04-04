8ecd272eae8e1659d504cf39485709f2fe82dd14
James Gough <james@peacefulprogramming.dev>
2025-04-04 21:16:11 +0100

    skeleton events mock

---
+++ pii_guardian/test/mocks/notion_event_mocks.ex
@@ -0,0 +1,8 @@
+defmodule PiiGuardian.SlackEventMocks do
+  @moduledoc """
+   Mocks for Notion events. These are used in tests to simulate Notion events.
+  """
+
+  def new_issue_event do
+  end
+end
