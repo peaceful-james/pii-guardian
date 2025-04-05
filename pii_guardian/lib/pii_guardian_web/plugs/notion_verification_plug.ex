533cf862e70e206c079d5c358c33c6a185537910
James Gough <james@peacefulprogramming.dev>
2025-04-06 00:35:00 +0100

    nicer logic for loggin notion verification token

--- pii_guardian/lib/pii_guardian_web/plugs/notion_verification_plug.ex
+++ pii_guardian/lib/pii_guardian_web/plugs/notion_verification_plug.ex
@@ -14,18 +14,24 @@ defmodule PiiGuardianWeb.Plugs.NotionVerificationPlug do
 
     ["sha256=" <> sha256_hash | _] = get_req_header(conn, "x-notion-signature")
     raw_body = conn.private[:raw_body]
-    Logger.info("Raw Notion post body: #{inspect(raw_body)}")
 
-    calculated_signature =
-      :hmac
-      |> :crypto.mac(:sha256, verification_token, raw_body)
-      |> Base.encode16(case: :lower)
+    case JSON.decode!(raw_body) do
+      %{"verification_token" => received_token} ->
+        Logger.info("Notion posted verification token: #{received_token}")
+        halt_with_ok(conn)
 
-    if Plug.Crypto.secure_compare(calculated_signature, sha256_hash) do
-      conn
-    else
-      Logger.warning("Notion webhook verification failed")
-      halt_with_unauthorized(conn)
+      _ ->
+        calculated_signature =
+          :hmac
+          |> :crypto.mac(:sha256, verification_token, raw_body)
+          |> Base.encode16(case: :lower)
+
+        if Plug.Crypto.secure_compare(calculated_signature, sha256_hash) do
+          conn
+        else
+          Logger.warning("Notion webhook verification failed")
+          halt_with_unauthorized(conn)
+        end
     end
   end
 
@@ -33,6 +39,13 @@ defmodule PiiGuardianWeb.Plugs.NotionVerificationPlug do
     conn
     |> put_resp_content_type("application/json")
     |> send_resp(401, Jason.encode!(%{error: "Unauthorized"}))
+    |> halt()
+  end
+
+  defp halt_with_ok(conn) do
+    conn
+    |> put_resp_content_type("application/json")
+    |> send_resp(200, Jason.encode!(%{ok: true}))
     |> halt()
   end
 end
