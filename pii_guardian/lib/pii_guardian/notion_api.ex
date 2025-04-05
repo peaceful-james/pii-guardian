ec88b6b59e76d28485ff7cb253c10c5500d3f1bf
James Gough <james@peacefulprogramming.dev>
2025-04-05 17:14:45 +0100

    get claude to generate a notion api module

---
+++ pii_guardian/lib/pii_guardian/notion_api.ex
@@ -0,0 +1,114 @@
+defmodule PiiGuardian.NotionApi do
+  @moduledoc """
+  Notion API client for interacting with the Notion API.
+
+  Provides functions to retrieve page content and delete pages.
+  """
+  use Tesla
+
+  require Logger
+
+  plug Tesla.Middleware.BaseUrl, "https://api.notion.com/v1"
+  plug Tesla.Middleware.JSON
+  plug Tesla.Middleware.BearerAuth, token: notion_token()
+  plug Tesla.Middleware.Headers, [{"Notion-Version", "2022-06-28"}]
+  plug Tesla.Middleware.Logger
+
+  @doc """
+  Retrieve a page by ID
+  https://developers.notion.com/reference/retrieve-a-page
+  """
+  def get_page(page_id) do
+    "/pages/#{page_id}"
+    |> get()
+    |> handle_response()
+  end
+
+  @doc """
+  Retrieve page content (blocks)
+  https://developers.notion.com/reference/retrieve-block-children
+  """
+  def get_page_content(page_id, start_cursor \\ nil, page_size \\ 100) do
+    query = [page_size: page_size]
+    query = if start_cursor, do: Keyword.put(query, :start_cursor, start_cursor), else: query
+
+    "/blocks/#{page_id}/children"
+    |> get(query: query)
+    |> handle_response()
+  end
+
+  @doc """
+  Delete (archive) a page - There is no direct API to delete a page, but we can archive it
+  https://developers.notion.com/reference/update-a-page
+  """
+  def delete_page(page_id) do
+    "/pages/#{page_id}"
+    |> patch(%{
+      "archived" => true
+    })
+    |> handle_response()
+  end
+
+  @doc """
+  Retrieve a block by ID
+  https://developers.notion.com/reference/retrieve-a-block
+  """
+  def get_block(block_id) do
+    "/blocks/#{block_id}"
+    |> get()
+    |> handle_response()
+  end
+
+  @doc """
+  Delete (archive) a block
+  https://developers.notion.com/reference/update-a-block
+  """
+  def delete_block(block_id) do
+    "/blocks/#{block_id}"
+    |> patch(%{
+      "archived" => true
+    })
+    |> handle_response()
+  end
+
+  @doc """
+  Recursively fetch all blocks for a page, handling pagination
+  """
+  def get_all_page_content(page_id) do
+    get_all_page_content_recursive(page_id, nil, [])
+  end
+
+  defp get_all_page_content_recursive(page_id, start_cursor, acc) do
+    case get_page_content(page_id, start_cursor) do
+      {:ok, %{"results" => results, "has_more" => true, "next_cursor" => next_cursor}} ->
+        get_all_page_content_recursive(page_id, next_cursor, acc ++ results)
+
+      {:ok, %{"results" => results}} ->
+        {:ok, acc ++ results}
+
+      {:error, reason} ->
+        {:error, reason}
+    end
+  end
+
+  defp handle_response({:ok, %{status: status, body: body}}) when status in 200..299 do
+    {:ok, body}
+  end
+
+  defp handle_response({:ok, %{status: status, body: body}}) do
+    Logger.error("Notion API HTTP error: #{status}, #{inspect(body)}")
+    {:error, "HTTP error #{status}: #{get_error_message(body)}"}
+  end
+
+  defp handle_response({:error, error}) do
+    Logger.error("Notion API client error: #{inspect(error)}")
+    {:error, "Client error: #{inspect(error)}"}
+  end
+
+  defp get_error_message(%{"message" => message}), do: message
+  defp get_error_message(_), do: "Unknown error"
+
+  defp notion_token do
+    Application.fetch_env!(:pii_guardian, :notion_token)
+  end
+end
